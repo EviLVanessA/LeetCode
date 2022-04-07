988d4e3e30a56679b9b921494462edfb43948a5b
jianghui <1242928326@qq.com>
2022-04-07 18:19:04 +0800

    第258题

--- src/com/study/leetcode201_300/LeetCode251_275/LeetCode258.java
+++ src/com/study/leetcode201_300/LeetCode251_275/LeetCode258.java
@@ -2,6 +2,23 @@ package com.study.leetcode201_300.LeetCode251_275;
 
 public class LeetCode258 {
     public static void main(String[] args) {
+        System.out.println(addDigits(10));
+    }
 
+    /**
+     * 使用循环
+     * @param num
+     * @return
+     */
+    public static int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num != 0) {
+                sum += num % 10;
+                num = num / 10;
+            }
+            num = sum;
+        }
+        return num;
     }
 }
