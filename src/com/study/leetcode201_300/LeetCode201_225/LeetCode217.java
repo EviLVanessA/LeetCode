3ed5a91fd59edb0b5784a1a1aae3c883861db77d
jianghui08 <jianghui08@58.com>
2022-05-24 14:37:35 +0800

    LeetCode217

--- src/com/study/leetcode201_300/LeetCode201_225/LeetCode217.java
+++ src/com/study/leetcode201_300/LeetCode201_225/LeetCode217.java
@@ -5,20 +5,20 @@ import java.util.Set;
 
 /**
  * 给定一个整数数组，判断是否存在重复元素。
- *
+ * <p>
  * 如果存在一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+ *
  * @author jianghui
  * @date 2021-02-01 11:11
  */
 public class LeetCode217 {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> ans = new HashSet<>();
-        for (int num : nums){
-            if (ans.contains(num)){
+        for (int num : nums) {
+            if (ans.contains(num)) {
                 return true;
-            }else {
-                ans.add(num);
             }
+            ans.add(num);
         }
         return false;
     }
