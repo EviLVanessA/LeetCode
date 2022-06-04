c6a047d76766fa512f24f6ba1352b91725fd37b4
jianghui <1242928326@qq.com>
2022-06-04 09:52:04 +0800

    LeetCode929

--- src/com/study/demo/Demo.java
+++ src/com/study/demo/Demo.java
@@ -5,37 +5,27 @@ package com.study.demo;
  * @date 2022/5/24
  */
 public class Demo {
-    public static boolean searchMatrix(int[][] matrix, int target) {
-        int row = matrix.length;
-        int col = matrix[0].length;
-        int left = 0, right = row - 1;
-        int pos = row - 1;
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (matrix[mid][0] <= target) {
-                pos = mid;
-                left = mid + 1;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
             } else {
-                right = mid - 1;
+                if (nums[nums.length - 1] >= target && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
-        left = 0;
-        right = col - 1;
-        int ans = col - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (matrix[pos][mid] >= target) {
-                ans = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return matrix[pos][ans] == target;
-    }
-
-    public static void main(String[] args) {
-        int[][] m = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
-        System.out.println(searchMatrix(m, 3));
+        return -1;
     }
 }
--- src/com/study/leetcode1_100/leetcode26_50/LeetCode33.java
+++ src/com/study/leetcode1_100/leetcode26_50/LeetCode33.java
@@ -6,11 +6,11 @@ package com.study.leetcode1_100.leetcode26_50;
  * 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
  * 你可以假设数组中不存在重复的元素。
  * 你的算法时间复杂度必须是 O(log n) 级别。
- *
+ * <p>
  * 示例 1:
  * 输入: nums = [4,5,6,7,0,1,2], target = 0
  * 输出: 4
- *
+ * <p>
  * 示例 2:
  * 输入: nums = [4,5,6,7,0,1,2], target = 3
  * 输出: -1
@@ -21,28 +21,28 @@ package com.study.leetcode1_100.leetcode26_50;
 public class LeetCode33 {
     public int search(int[] nums, int target) {
         int n = nums.length;
-        if (n == 0){
+        if (n == 0) {
             return -1;
         }
-        if (n == 1){
+        if (n == 1) {
             return nums[0] == target ? 0 : -1;
         }
-        int l = 0 , r = n - 1;
-        while (l <= r){
+        int l = 0, r = n - 1;
+        while (l <= r) {
             int mid = l + (r - l) / 2;
-            if (nums[mid] == target){
+            if (nums[mid] == target) {
                 return mid;
             }
-            if (nums[0] <= nums[mid]){
-                if (nums[0] <= target && target < nums[mid]){
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) {
                     r = mid - 1;
-                }else {
+                } else {
                     l = mid + 1;
                 }
-            }else {
-                if (nums[mid] < target && target <= nums[n-1]){
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
                     l = mid + 1;
-                }else {
+                } else {
                     r = mid - 1;
                 }
             }
